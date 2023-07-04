500772ad2008cedce0c19ab6bf991175f0be5545
kafe1994 <liendoalejandro94@gmail.com>
2023-07-04 14:19:46 -0300

    tarea Openbootcamp

---
+++ tarea2.py
@@ -0,0 +1,7 @@
+#ejercicio numero 3 de openbootcamp
+
+print("bienvenido al pequenio programa")
+peso = int(input("introduce tu peso en kilogramos "))
+estatura = float((input("introduce tu estatura con decimales ")))
+total = peso / (estatura * estatura)
+print("tu masa corporal es de", round(total,2))
